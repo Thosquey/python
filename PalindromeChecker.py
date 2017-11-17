59e6302501a44b91000b2761d55685bbc22fbe1d
Thosquey <clement.guibout@gmail.com>
2017-11-17 07:42:19 +0100

    Update PalindromeChecker.py

--- PalindromeChecker.py
+++ PalindromeChecker.py
@@ -5,5 +5,5 @@ while True:
         if reversedString[::-1] == reversedString:
             return initialString + " est un mot palindrome"
         else:
-            return initialString +  " n'est pas un mot palindrome (l'inverser donne : " + initialString[::-1] + " ce qui n'est pas pareil que " + initialString + ")"
+            return initialString +  " n'est pas un mot palindrome"
     print isPalindrome(reversedString) + "\n"
