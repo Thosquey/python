f6a3cd8392b873acc4de973613e466d575086973
Thosquey <clement.guibout@gmail.com>
2017-11-19 16:05:40 +0100

    Delete PalindromeChecker.py

--- PalindromeChecker.py
+++
@@ -1,9 +0,0 @@
-while True:
-    initialString = raw_input("Entrez un mot : ")
-    reversedString = initialString.lower()
-    def isPalindrome(reversedString):
-        if reversedString[::-1] == reversedString:
-            return initialString + " est un mot palindrome"
-        else:
-            return initialString +  " n'est pas un mot palindrome"
-    print isPalindrome(reversedString) + "\n"
