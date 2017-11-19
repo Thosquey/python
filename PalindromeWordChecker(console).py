aa9eb6a3013d76cffac93f7d9d841945adbff0e3
Thosquey <clement.guibout@gmail.com>
2017-11-19 18:29:51 +0100

    Update PalindromeWordChecker(console).py

--- PalindromeWordChecker(console).py
+++ PalindromeWordChecker(console).py
@@ -1,9 +1,9 @@
 while True:
-    initialString = raw_input("Entrez un mot : ")
-    reversedString = initialString.lower()
-    def isPalindrome(reversedString):
-        if reversedString[::-1] == reversedString:
-            return initialString + " est un mot palindrome"
+    initialString = raw_input("\nEnter a word : ")
+    loweredString = initialString.lower()
+    def isPalindrome(initialString):
+        if loweredString[::-1] == loweredString:
+            return initialString + " is a palindrome word"
         else:
-            return initialString +  " n'est pas un mot palindrome"
-    print isPalindrome(reversedString) + "\n"
+            return initialString + " isn't a palindrome word"
+    print isPalindrome(initialString)
