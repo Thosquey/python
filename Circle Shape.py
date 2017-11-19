fbe65f56ceaf56a64c64101b5d269c315a324182
Thosquey <clement.guibout@gmail.com>
2017-11-19 22:17:24 +0100

    Create Circle Shape.py

---
+++ Circle Shape.py
@@ -0,0 +1,11 @@
+import turtle
+for i in range(250):
+    turtle.speed(500000)
+    turtle.forward(250)
+    turtle.right(90)
+    turtle.forward(250)
+    turtle.right(90)
+    turtle.forward(250)
+    turtle.right(90)
+    turtle.forward(250)
+    turtle.right(3)
